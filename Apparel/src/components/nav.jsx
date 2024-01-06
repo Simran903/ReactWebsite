570d44877d7b5f7db7b109a325dd16fb697801dc
Simran <itssimran903@gmail.com>
2024-01-06 16:16:03 +0530

    commit

--- Apparel/src/components/nav.jsx
+++ Apparel/src/components/nav.jsx
@@ -1,38 +1,47 @@
 import React from "react";
 import { navLinks } from "../constant";
+import { headerLogo } from "../assets/images";
+import { hamburger } from '../assets/icons'
 
 function Nav() {
-  return (
-    <header
-      className="padding-x
+    return (
+        <header
+            className="padding-x
         py-8 absolute z-10 w-full"
-    >
-      <nav
-        className="flex
+        >
+            <nav
+                className="flex
             justify-between items-center
             max-container"
-      >
-        <ul
-          className="flex-1 flex justify-center items-center
+            >
+                <a href="/">
+                    <img src={headerLogo} alt="LOGO" />
+                </a>
+                <ul
+                    className="flex-1 flex justify-center items-center
                 gap-16 max-lg:hidden"
-        >
-          {navLinks.map((item) => (
-            <li key={item.id}>
-              <a
-                href={item.href}
-                className="font-montserrat
+                >
+                    {navLinks.map((item) => (
+                        <li key={item.id}>
+                            <a
+                                href={item.href}
+                                className="font-montserrat
                             leading-normal
                             text-lg
                             text-slate-gray"
-              >
-                {item.label}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+                            >
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+                <div className='hidden 
+        max-lg:block'>
+                    <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+                </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Nav;
